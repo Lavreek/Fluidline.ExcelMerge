6bfa415454b545368c406a067d5e95f18ab9ff54
adds111 <mr.mikhail.ya@gmail.com>
2023-10-20 12:46:52 +0300

    ura

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    $files = Storage::Files('/exel');
+    return view('welcome',['files' => $files]);
+});
+Route::post('/upload', '\App\Http\Controllers\ExelController@upload');
+Route::get('/Download/', '\App\Http\Controllers\ExelController@download');
