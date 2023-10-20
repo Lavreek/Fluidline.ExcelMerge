47087f02d5a5fbbe448c69f61c7d329590f881d7
adds111 <mr.mikhail.ya@gmail.com>
2023-10-20 16:16:59 +0300

    oi5

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,10 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-        <script src='../../js/form.js'></script>
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <link rel="stylesheet" href="../../css/app.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     </head>
     <body id="body">
